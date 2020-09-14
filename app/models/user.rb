757359b62c43a0a54565292eea0c609d33c72ded
Ohtsuka Takahiro <t.o.0526@gmail.com>
2020-09-14 14:31:03 +0900

    6-4-4 イベント登録用のフォームを作る

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  hasmany :created_events, class_name: "Event", foreign_key: "owner_id"
+  has_many :created_events, class_name: "Event", foreign_key: "owner_id"
 
   def self.find_or_create_from_auth_hash!(auth_hash)
     provider = auth_hash[:provider]

e125f0530bd013fa95850ae3fa7d2e995742b4b4
Ohtsuka Takahiro <t.o.0526@gmail.com>
2020-09-18 06:16:58 +0900

    SimpleCov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
+
+require "simplecov"
+SimpleCov.start "rails"
+
 require_relative '../config/environment'
 require_relative 'sign_in_helper'
 require 'rails/test_help'
@@ -6,7 +10,7 @@ require 'minitest/mock'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  # parallelize(workers: :number_of_processors)
 
   # Add more helper methods to be used by all tests here...
 end

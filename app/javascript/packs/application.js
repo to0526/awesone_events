6b1e8e8c7d7c7947bef3ef217bae41d7f7ac5fc9
Ohtsuka Takahiro <t.o.0526@gmail.com>
2020-09-17 11:21:52 +0900

    Searchkickでイベント検索機能を作る

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("get_form_turbolinks");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,5 +15,5 @@ require("@rails/activestorage").start()
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import "bootstrap"
-import "bootstrap/scss/bootstrap.scss"
+import "bootstrap";
+import "bootstrap/scss/bootstrap.scss";
